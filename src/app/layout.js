a0274abb60be2d29dfcabc4a963c6a93aad2376a
monishas-proz <136665639+monishas-proz@users.noreply.github.com>
2026-07-24 10:54:32 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,22 @@
-import { Hanken_Grotesk, Montserrat, DM_Sans } from "next/font/google";
+// import { Hanken_Grotesk, Montserrat, DM_Sans } from "next/font/google";
 import Header from "@/components/Header";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
-const hankenGrotesk = Hanken_Grotesk({
-  variable: "--font-hanken-grotesk",
-  subsets: ["latin"],
-});
+// const hankenGrotesk = Hanken_Grotesk({
+//   variable: "--font-hanken-grotesk",
+//   subsets: ["latin"],
+// });
 
-const montserrat = Montserrat({
-  variable: "--font-montserrat",
-  subsets: ["latin"],
-});
+// const montserrat = Montserrat({
+//   variable: "--font-montserrat",
+//   subsets: ["latin"],
+// });
 
-const dmSans = DM_Sans({
-  variable: "--font-dm-sans",
-  subsets: ["latin"],
-});
+// const dmSans = DM_Sans({
+//   variable: "--font-dm-sans",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Air Care",
@@ -26,9 +26,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${hankenGrotesk.variable} ${montserrat.variable} ${dmSans.variable}`}
-      >
+      // <body
+      //   className={`${hankenGrotesk.variable} ${montserrat.variable} ${dmSans.variable}`}
+      // >
+    <body>
         <Header />
         <main>{children}</main>
         <Footer />
