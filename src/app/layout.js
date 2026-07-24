8da91b3badf8b70d503f141ecb4bafe8443b3ab8
monishas-proz <136665639+monishas-proz@users.noreply.github.com>
2026-07-24 10:59:12 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,9 +26,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      // <body
-      //   className={`${hankenGrotesk.variable} ${montserrat.variable} ${dmSans.variable}`}
-      // >
+     
       <body>
         <Header />
         <main>{children}</main>
